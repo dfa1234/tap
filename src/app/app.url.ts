07a1ccd4116c323080f8ef63520e99c34daaf7c7
Yossi <yosyospardo@gmail.com>
2018-01-07 17:51:41 +0200

    fixing

--- src/app/app.url.ts
+++ src/app/app.url.ts
@@ -7,10 +7,10 @@ export class BaseUrl {
   constructor() {
   }
 
-    baseUrl = 'http://vps160905.vps.ovh.ca:3000';
+    baseUrl = 'http://localhost:3000';
     header = new HttpHeaders({
         'Access-Control-Allow-Credentials':'true',
-        'Access-Control-Allow-Origin' : 'http://vps160905.vps.ovh.ca',
+        'Access-Control-Allow-Origin' : 'http://localhost:8100',
         'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE',
         'Access-Control-Allow-Headers' : 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept,Access-Control-Allow-Credentials,Authorization'
     });
